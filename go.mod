ec83d8b7ff44c9ae84f637f2b90a48a8b31c214f
Kevin Lee <klee03509@gmail.com>
2021-06-27 02:46:00 -0700

    Simplify project even more

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/kevhlee/chip8
 go 1.15
 
 require (
-	github.com/MakeNowJust/heredoc v1.0.0
 	github.com/hajimehoshi/ebiten/v2 v2.0.4
 	github.com/spf13/cobra v1.1.1
 )
--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/MakeNowJust/heredoc"
 	"github.com/kevhlee/chip8/ch8"
 	"github.com/spf13/cobra"
 )
@@ -13,58 +12,9 @@ import (
 func main() {
 	cli := &cobra.Command{
 		Use:     "ch8",
-		Example: heredoc.Doc(`$ ch8 run roms/Logo.ch8`),
-		Long: heredoc.Doc(
-			`
-
-			 ██████╗██╗  ██╗██╗██████╗        █████╗ 
-			██╔════╝██║  ██║██║██╔══██╗      ██╔══██╗
-			██║     ███████║██║██████╔╝█████╗╚█████╔╝
-			██║     ██╔══██║██║██╔═══╝ ╚════╝██╔══██╗
-			╚██████╗██║  ██║██║██║           ╚█████╔╝
-			 ╚═════╝╚═╝  ╚═╝╚═╝╚═╝            ╚════╝
-
-			A CHIP-8 emulator written in Go.
-			`,
-		),
-	}
-
-	cli.AddCommand(newRunCmd())
-
-	if err := cli.Execute(); err != nil {
-		fmt.Println(err)
-	}
-}
-
-func newRunCmd() *cobra.Command {
-	runCmd := &cobra.Command{
-		Use:   "run <path to ROM>",
-		Short: "Run a CHIP-8 ROM file",
-		Args: func(cmd *cobra.Command, args []string) error {
-			if len(args) < 1 {
-				return fmt.Errorf("input a path to a CHIP-8 ROM file")
-			}
-
-			if _, err := os.Stat(args[0]); os.IsNotExist(err) {
-				return err
-			}
-
-			scale, err := cmd.Flags().GetInt("scale")
-			if err != nil {
-				return err
-			} else if scale < 1 {
-				return errors.New("scale factor must be positive")
-			}
-
-			volume, err := cmd.Flags().GetFloat64("volume")
-			if err != nil {
-				return err
-			} else if volume < 0.0 || volume > 1.0 {
-				return errors.New("volume must be between [0.0, 1.0]")
-			}
-
-			return nil
-		},
+		Example: "$ ch8 roms/Logo.ch8",
+		Long:    "A CHIP-8 emulator written in Go.",
+		Args:    checkArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			scale, _ := cmd.Flags().GetInt("scale")
 			volume, _ := cmd.Flags().GetFloat64("volume")
@@ -79,18 +29,51 @@ func newRunCmd() *cobra.Command {
 		},
 	}
 
-	runCmd.Flags().IntP(
+	addFlags(cli)
+
+	if err := cli.Execute(); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func addFlags(cli *cobra.Command) {
+	cli.Flags().IntP(
 		"scale",
 		"s",
 		ch8.DefaultScale,
 		"set the scale factor of the CHIP-8 screen",
 	)
-	runCmd.Flags().Float64P(
+
+	cli.Flags().Float64P(
 		"volume",
 		"v",
 		ch8.DefaultVolume,
 		"set the volume of the CHIP-8 emulator",
 	)
+}
 
-	return runCmd
+func checkArgs(cli *cobra.Command, args []string) error {
+	if len(args) < 1 {
+		return fmt.Errorf("input a path to a CHIP-8 ROM file")
+	}
+
+	if _, err := os.Stat(args[0]); os.IsNotExist(err) {
+		return err
+	}
+
+	scale, err := cli.Flags().GetInt("scale")
+	if err != nil {
+		return err
+	} else if scale < 1 {
+		return errors.New("scale factor must be positive")
+	}
+
+	volume, err := cli.Flags().GetFloat64("volume")
+	if err != nil {
+		return err
+	} else if volume < 0.0 || volume > 1.0 {
+		return errors.New("volume must be between [0, 1]")
+	}
+
+	return nil
 }
