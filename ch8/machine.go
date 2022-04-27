480962a69fed6707dea3205ef08309d8bef6aba4
Kevin Lee <klee03509@gmail.com>
2022-04-27 00:30:11 -0700

    Revert constants file removal

--- ch8/machine.go
+++ ch8/machine.go
@@ -6,48 +6,6 @@ import (
 	"math/rand"
 )
 
-const (
-	// FontSize is the number of bytes in a CHIP-8 built-in font.
-	FontSize = 0x5
-
-	// MaxStackDepth is the maximum depth of the CHIP-8 virtual
-	// machine's call stack.
-	MaxStackDepth = 0x10
-
-	// MemorySize is the total amount of memory available in the CHIP-8
-	// virtual machine.
-	MemorySize = 0x1000
-
-	// DisplayWidth is the width (in pixels) of the CHIP-8 display.
-	DisplayWidth = 0x40
-
-	// DisplayHeight is the height (in pixels) of the CHIP-8 display.
-	DisplayHeight = 0x20
-
-	// NumberOfKeys is the number of keys in the CHIP-8 keyboard.
-	NumberOfKeys = 0x10
-
-	// NumberOfFonts is the total number of built-in fonts in the
-	// CHIP-8 virtual machine.
-	NumberOfFonts = 0x10
-
-	// NumberOfPixels is the total number of pixels in the CHIP-8
-	// display.
-	NumberOfPixels = DisplayWidth * DisplayHeight
-
-	// NumberOfRegisters is the number of general-purpose registers in
-	// the CHIP-8 virtual machine.
-	NumberOfRegisters = 0x10
-
-	// ProgramStartAddress is the start memory location where programs
-	// are loaded in the virtual machine's memory.
-	ProgramStartAddress = 0x200
-
-	// ProgramMemorySize is the total amount of memory available for
-	// CHIP-8 programs.
-	ProgramMemorySize = MemorySize - ProgramStartAddress
-)
-
 //===========================================================================
 // Errors
 //===========================================================================
