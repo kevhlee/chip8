2369a890f6633b8a0c60e7d88101e8d351763034
Kevin Lee <klee03509@gmail.com>
2025-02-02 20:52:28 -0800

    Match key layout to the hexadecimal keypad

--- chip8/interpreter.go
+++ chip8/interpreter.go
@@ -10,10 +10,10 @@ import (
 
 var (
 	scancodeHexMap map[sdl.Scancode]uint8 = map[sdl.Scancode]uint8{
-		sdl.SCANCODE_1: 0x0, sdl.SCANCODE_2: 0x1, sdl.SCANCODE_3: 0x2, sdl.SCANCODE_4: 0x3,
-		sdl.SCANCODE_Q: 0x4, sdl.SCANCODE_W: 0x5, sdl.SCANCODE_E: 0x6, sdl.SCANCODE_R: 0x7,
-		sdl.SCANCODE_A: 0x8, sdl.SCANCODE_S: 0x9, sdl.SCANCODE_D: 0xA, sdl.SCANCODE_F: 0xB,
-		sdl.SCANCODE_Z: 0xC, sdl.SCANCODE_X: 0xD, sdl.SCANCODE_C: 0xE, sdl.SCANCODE_V: 0xF,
+		sdl.SCANCODE_1: 0x1, sdl.SCANCODE_2: 0x2, sdl.SCANCODE_3: 0x3, sdl.SCANCODE_4: 0xC,
+		sdl.SCANCODE_Q: 0x4, sdl.SCANCODE_W: 0x5, sdl.SCANCODE_E: 0x6, sdl.SCANCODE_R: 0xD,
+		sdl.SCANCODE_A: 0x7, sdl.SCANCODE_S: 0x8, sdl.SCANCODE_D: 0x9, sdl.SCANCODE_F: 0xE,
+		sdl.SCANCODE_Z: 0xA, sdl.SCANCODE_X: 0x0, sdl.SCANCODE_C: 0xB, sdl.SCANCODE_V: 0xF,
 	}
 )
 
